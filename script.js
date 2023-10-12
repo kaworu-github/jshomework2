b522c9dadb3aa265158c38f9627221aa8952e686
github kaworu <130183111+kaworu-github@users.noreply.github.com>
2023-10-12 19:32:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,177 @@
+/* 1 */
+
+let test = 250;
+
+if (test > 100 && test < 500) {
+  console.log(true);
+}else{
+  console.log(false);
+}
+
+
+
+
+/* 2 */
+
+let test1 = 32;
+
+if (test1 > 20 && test1 < 58) {
+  console.log("narmul");
+}else{
+  console.log(false);
+}
+
+
+/* 3 */
+
+let test2 = 41;
+
+if (test2 > 0 && test2 < 33) {
+  console.log("klasno");
+}else{
+  console.log(false);
+}
+
+
+/* 4 */
+
+let test3 = 29;
+
+if (test3 > 0 && test3 < 64) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+
+/* 5 */
+
+let test4 = 82;
+
+if (test4 > 1 && test4 < 14) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+
+
+/* 6 */
+
+let test5 = 52;
+
+if (test5 > 9 && test5 < 23) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+
+/* 7 */
+
+let str = "Hello world"
+
+if (str.length > 5 && str.length < 20) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+
+/* 8 */
+if (str.length > 0 && str.length < 76) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+
+/* 9 */
+
+if (str.length > 10 && str.length < 40) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+
+/* 10 */
+
+if (str.length > 1 && str.length < 10) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+
+/* /* 11 */
+
+/* let number = prompt()
+
+if (number > 0 && number < 50) {
+  console.log(number);
+} else if (number > 51 && number < 100) {
+  console.log(true);
+}else if (number > 101) {
+  console.log(false);
+}
+ */
+
+
+
+
+/* 12 */
+
+/* let string = prompt();
+
+if (string.length > 0 && string.length < 10) {
+  console.log(string.length);
+}else if (string.length > 11 && string.length < 20) {
+  console.log(true);
+}else if (string.length > 21) {
+  console.log(false);
+} */
+
+
+
+
+/* 13 */
+
+
+const arr = [true, "Hello", null, 19, undefined];
+
+if (arr.includes(10)) {
+  console.log(10);
+}else if (arr == true) {
+  console.log(true);
+}else if (arr.includes("Hello")) {
+  console.log(false);
+}
+
+
+
+/* 14 */
+
+const array = [true, false, null, undefined, "JavaScript", 12];
+
+if (array.length > 5 && array.length < 10) {
+  console.log("Успіх");
+}else if (array.length > 10 && array.length < 20) {
+  console.log(true);
+}else if (array.length < 5) {
+  console.log(false);
+}
+
+
+
+/* 15 */
+
+let stroka = "джаваскрипт";
+
+if (stroka.includes("к") && stroka.includes("а")) {
+  console.log("це Успіх");
+}else if (stroka.includes("а") && stroka.includes("п")) {
+  console.log(true);
+}else if (stroka.includes("а") && stroka.includes("о")) {
+  console.log(false);
+}
